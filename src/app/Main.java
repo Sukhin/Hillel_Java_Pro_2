ece971f93120315115f9c2dddaa0a51b8066787f
Vitaly_Sukhin <VVS11101992@gmail.com>
2026-04-10 16:34:22 +0300

    Added method converting miles in kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,9 +2,18 @@ package app;
 
 public class Main {
 
+    private static final double CONV_K = 1.60934;
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to the program for converting units measuring distance!");
+        double miInitial = 5;
+        double km = convMiToKm(miInitial);
+        System.out.printf("Result: %.3f mile(s) = %.3f kilometer(s).", miInitial, km);
+    }
+
+    private static double convMiToKm(double miInitial) {
+        return miInitial * CONV_K;
 
     }
 
